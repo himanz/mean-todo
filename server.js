7abe9cecf0210a2fb91baf10a20495c238db387b
Jonathan Chang <jonoschang7@gmail.com>
2014-11-10 16:10:49 -0500

    fix syntax error

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.post('/api/todos', function(req, res) {
 app.delete('/api/todos/:todo_id', function(req, res) {
   Todo.remove({
   	_id : req.params.todo_id
-  }, function(err. todo) {
+  }, function(err, todo) {
     if (err)
     	res.send(err);
 
