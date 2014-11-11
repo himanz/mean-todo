ff6a30ce1d4216fbb6d6a1b085efaf6649c17fc5
Jonathan Chang <jonoschang7@gmail.com>
2014-11-11 05:10:05 -0500

    fix syntax error

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Todo', {
-	text : String
+	text : String,
 	done : Boolean
 });
--- server.js
+++ server.js
@@ -1,36 +1,26 @@
-// Set up
-var express = require('express');
-// create app with express
-var app = express();
-// mongoose for mongodb
-var mongoose = require('mongoose');
-// log requests to the console (express4)
-var morgan = require('morgan');
-// pull information from HTML POST (express4)
-var bodyParser = require('body-parser');
-// simulate DELETE and PUT (express 4)
-var methodOverride = require('method-override');
+ // set up
+    var express  = require('express');
+    var app      = express();                               // create our app w/ express
+    var mongoose = require('mongoose');                     // mongoose for mongodb
+    var port     = process.env.PORT || 8080;                // set the port
+    var database = require('./config/database');            // load the database config
+        var morgan = require('morgan');             // log requests to the console (express4)
+    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
-// configuration
-// load the database
-var database = require('./config/database')
-//  connect to mongoDB database on modulus.io
-mongoose.connect(database.url);
-// set the static files location
-app.use(express.static(__dirname + '/public'));
-// log every request to the console
-app.use(morgan('dev'));
-// parse applicaton/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({'extended':'true'}));
-// parse application/json
-app.use(bodyParser.json());
-// parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use(methodOverride());
+    // configuration
+    mongoose.connect(database.url);     // connect to mongoDB database on modulus.io
 
-// load the routes
-require('./app/routes')(app);
+    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+    app.use(morgan('dev'));                                         // log every request to the console
+    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+    app.use(bodyParser.json());                                     // parse application/json
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+    app.use(methodOverride());
 
-// listen
-app.listen(8080);
-console.log("App listening on port 8080");
+    // routes
+    require('./app/routes.js')(app);
+
+    // listen (start app with node server.js) 
+    app.listen(port);
+    console.log("App listening on port " + port);
