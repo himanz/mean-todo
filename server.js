c95172ca20419f7027a5b1f7f763707cca60c02e
Jonathan Chang <jonoschang7@gmail.com>
2014-11-11 05:01:11 -0500

    move routes from server.js to routes.js

--- server.js
+++ server.js
@@ -28,60 +28,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
-// routes
-
-// api
-// get all todos
-app.get('/api/todos', function(req, res) {
-  Todo.find(function(err, todos) {
-  	// if there is an error retrieving, send the error. 
-  	if (err)
-  		res.send(err)
-  	res.json(todos);  // return all todos in JSON format
-  });
-});
-
-// create todo and send back all todos after creation
-app.post('/api/todos', function(req, res) {
-  // create a todo, information comes from AJAX request from Angular
-  Todo.create({
-    text : req.body.text,
-    done : false
-  }, function(err, todo) {
-  	if (err)
-  		res.send(err);
-
-  	// get and return all todos after you create another
-  	Todo.find(function(err, todos) {
-  		if (err)
-  			res.send(err)
-  		res.json(todos);
-  	});
-  });
-});
-
-
-app.delete('/api/todos/:todo_id', function(req, res) {
-  Todo.remove({
-  	_id : req.params.todo_id
-  }, function(err, todo) {
-    if (err)
-    	res.send(err);
-
-    // get and return all the todos after you create another
-    Todo.find(function(err, todos) {
-    	if (err)
-    		res.send(err)
-    	res.json(todos);
-    });
-  });
-});
-
-// application
-
-app.get('*', function(req, res) {
-  res.sendfile('./public/index.html') // load the single view files
-});
+// load the routes
+require('./app/routes')(app);
 
 // listen
 app.listen(8080);
